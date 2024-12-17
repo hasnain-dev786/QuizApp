8e1fdbe3cc3db0addb5095ec7d00f2864dae9aa7
Hasnain Amjad <hasnainamjad467@gmail.com>
2024-12-17 10:58:45 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+let allQuestions = [
+    {
+        question :"CPU stands for ?",
+        options :[
+            
+                {answer:"Central Processing Unit",iscorrect:true},
+                {answer:"Central Process Unit",iscorrect:false},
+                {answer:"Central Processing Unif",iscorrect:false},
+                {answer:"Central Programming Unit",iscorrect:false}
+    
+            
+        ]
+
+    },
+
+    {
+        question :"JS stands for ?",
+        options :[
+            
+                {answer:"Java Scrupt",iscorrect:false},
+                {answer:"Java Script",iscorrect:true},
+                {answer:"Json",iscorrect:false},
+                {answer:"Javasciennce",iscorrect:false}
+    
+            
+        ]
+    },
+    {
+        question :"html stands for ?",
+        options :[
+            
+                {answer:"hyper text markup lesson",iscorrect:false},
+                {answer:"hyper text makeup langauage",iscorrect:false},
+                {answer:"Json",iscorrect:false},
+                {answer:"hyper text markup langauage",iscorrect:true}
+    
+            
+        ]
+    },
+    {
+        question :" what is mysql ?",
+        options :[
+            
+                {answer:"database",iscorrect:true},
+                {answer:"hyper text makeup langauage",iscorrect:false},
+                {answer:"Json",iscorrect:false},
+                {answer:"backend program",iscorrect:false}
+    
+            
+        ]
+    },
+    {
+        question :"css stands for ",
+        options :[
+            
+                {answer:"hyper text markup lesson",iscorrect:false},
+                {answer:"hyper text makeup langauage",iscorrect:false},
+                {answer:"cascading stylesheet",iscorrect:true},
+                {answer:"hyper text markup langauage",iscorrect:false}
+    
+            
+        ]
+    }
+
+]
+console.log(allQuestions);
+
+
